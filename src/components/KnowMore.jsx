8b41cd5e4537e6dfc457e94b03f4a75a18b329a7
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-11-24 14:40:10 +0100

    Merge branch 'master' of https://github.com/amorea9/advice-react-case

--- src/components/KnowMore.jsx
+++ src/components/KnowMore.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-function KnowMore() {
+function KnowMore(props) {
   return (
     <div className="UI-container">
       <h1>How do we know?</h1>
-      <p>It takes about (X.X)g of CO2 to produce a cookie. Each google search emits about (XX)g of CO2.</p>
-      <p>In the algorithm used, X is cookies and y is the searches:</p>
-
-      <blockquote>algorithm here</blockquote>
-      <button>Home</button>
+      <p>It takes about (1.38)g of CO2 to produce a cookie. Each google search emits about (0.2)g of CO2.</p>
+      {/* <p>In the algorithm used, C stands for the CO2 that the production of a single cookie emits and S is the number of searches:</p> */}
+      <p>In the algorithm used, "Cookies" stands for the number of cookies and "Search" is the number of searches that you've submitted :</p>
+      <blockquote> Cookies = (Searches * 0.2g) / 1.38g </blockquote>
+      <button onClick={props.getLanding}>Home</button>
     </div>
   );
 }
