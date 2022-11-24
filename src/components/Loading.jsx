dad9b25ebac9204b305ee420bd9ce51412c5e6b8
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-11-24 21:23:09 +0100

    oven loader fixes

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,70 +2,70 @@ import React from "react";
 
 function Loading() {
   return (
-    <>
-      <svg className="oven" width="204" height="204" viewBox="0 0 204 204" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <mask id="path-1-inside-1_116_16" fill="white">
-          <rect y="13" width="204" height="178" rx="3" />
+    <div className="UI-container">
+      <svg className="oven" width="204" height="201" viewBox="0 0 204 201" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <mask id="path-1-inside-1_129_3" fill="white">
+          <rect y="10" width="204" height="178" rx="3" />
         </mask>
-        <rect y="13" width="204" height="178" rx="3" fill="#D7D7D7" />
-        <rect y="13" width="204" height="178" rx="3" fill="url(#paint0_linear_116_16)" />
-        <rect y="13" width="204" height="178" rx="3" stroke="#0F0F0F" strokeWidth="10" mask="url(#path-1-inside-1_116_16)" />
-        <mask id="path-2-inside-2_116_16" fill="white">
-          <rect x="23" y="90" width="157" height="80" rx="3" />
+        <rect y="10" width="204" height="178" rx="3" fill="#EAEAEA" />
+        <rect y="10" width="204" height="178" rx="3" fill="url(#paint0_linear_129_3)" />
+        <rect y="10" width="204" height="178" rx="3" stroke="#0F0F0F" stroke-width="10" mask="url(#path-1-inside-1_129_3)" />
+        <mask id="path-2-inside-2_129_3" fill="white">
+          <rect x="23" y="87" width="157" height="80" rx="3" />
         </mask>
-        <rect x="23" y="90" width="157" height="80" rx="3" fill="#FFB72A" />
-        <rect className="oven_inside" x="23" y="90" width="157" height="80" rx="3" fill="url(#paint1_linear_116_16)" fillOpacity="0.5" />
-        <rect x="23" y="90" width="157" height="80" rx="3" stroke="#823C15" strokeWidth="10" mask="url(#path-2-inside-2_116_16)" />
-        <mask id="path-3-inside-3_116_16" fill="white">
-          <rect x="23" y="131" width="157" height="39" rx="3" />
+        <rect x="23" y="87" width="157" height="80" rx="3" fill="#FFB72A" />
+        <rect className="oven_inside" x="23" y="87" width="157" height="80" rx="3" fill="url(#paint1_linear_129_3)" fill-opacity="0.5" />
+        <rect x="23" y="87" width="157" height="80" rx="3" stroke="black" stroke-width="10" mask="url(#path-2-inside-2_129_3)" />
+        <mask id="path-3-inside-3_129_3" fill="white">
+          <rect x="126" y="121" width="36" height="12" rx="3" />
         </mask>
-        <rect x="23" y="131" width="157" height="39" rx="3" stroke="#823C15" strokeWidth="10" mask="url(#path-3-inside-3_116_16)" />
-        <mask id="path-4-inside-4_116_16" fill="white">
-          <rect x="126" y="124" width="36" height="12" rx="3" />
+        <rect x="126" y="121" width="36" height="12" rx="3" fill="#E1B45C" stroke="#503603" stroke-width="10" mask="url(#path-3-inside-3_129_3)" />
+        <mask id="path-4-inside-4_129_3" fill="white">
+          <rect x="84" y="121" width="36" height="12" rx="3" />
         </mask>
-        <rect x="126" y="124" width="36" height="12" rx="3" fill="#E1B45C" stroke="#782C01" strokeWidth="10" mask="url(#path-4-inside-4_116_16)" />
-        <mask id="path-5-inside-5_116_16" fill="white">
-          <rect x="84" y="124" width="36" height="12" rx="3" />
+        <rect x="84" y="121" width="36" height="12" rx="3" fill="#E1B45C" stroke="#503603" stroke-width="10" mask="url(#path-4-inside-4_129_3)" />
+        <mask id="path-5-inside-5_129_3" fill="white">
+          <rect x="42" y="121" width="36" height="12" rx="3" />
         </mask>
-        <rect x="84" y="124" width="36" height="12" rx="3" fill="#E1B45C" stroke="#782C01" strokeWidth="10" mask="url(#path-5-inside-5_116_16)" />
-        <mask id="path-6-inside-6_116_16" fill="white">
-          <rect x="42" y="124" width="36" height="12" rx="3" />
+        <rect x="42" y="121" width="36" height="12" rx="3" fill="#E1B45C" stroke="#503603" stroke-width="10" mask="url(#path-5-inside-5_129_3)" />
+        <mask id="path-6-inside-6_129_3" fill="white">
+          <rect x="14" y="72" width="176" height="20" rx="3" />
         </mask>
-        <rect x="42" y="124" width="36" height="12" rx="3" fill="#E1B45C" stroke="#782C01" strokeWidth="10" mask="url(#path-6-inside-6_116_16)" />
-        <mask id="path-7-inside-7_116_16" fill="white">
-          <rect x="14" y="75" width="176" height="20" rx="3" />
+        <rect x="14" y="72" width="176" height="20" rx="3" fill="#BDBDBD" stroke="black" stroke-width="10" mask="url(#path-6-inside-6_129_3)" />
+        <mask id="path-7-inside-7_129_3" fill="white">
+          <rect y="10" width="204" height="47" rx="3" />
         </mask>
-        <rect x="14" y="75" width="176" height="20" rx="3" fill="#BDBDBD" stroke="#3A3A3A" strokeWidth="10" mask="url(#path-7-inside-7_116_16)" />
-        <mask id="path-8-inside-8_116_16" fill="white">
-          <rect y="13" width="204" height="47" rx="3" />
+        <rect y="10" width="204" height="47" rx="3" fill="#EAEAEA" stroke="#0F0F0F" stroke-width="10" mask="url(#path-7-inside-7_129_3)" />
+        <rect x="117.5" y="2.5" width="60" height="9" rx="3.5" fill="#5F5F5F" stroke="black" stroke-width="5" />
+        <rect x="25.5" y="2.5" width="60" height="9" rx="3.5" fill="#5F5F5F" stroke="black" stroke-width="5" />
+        <mask id="path-10-inside-8_129_3" fill="white">
+          <rect x="168" y="184" width="26" height="17" rx="3" />
         </mask>
-        <rect y="13" width="204" height="47" rx="3" fill="#BCBCBC" stroke="#0F0F0F" strokeWidth="10" mask="url(#path-8-inside-8_116_16)" />
-        <rect x="117.5" y="2.5" width="60" height="12" rx="3.5" fill="#BDBDBD" stroke="#0F0F0F" strokeWidth="5" />
-        <rect x="25.5" y="2.5" width="60" height="12" rx="3.5" fill="#BDBDBD" stroke="#0F0F0F" strokeWidth="5" />
-        <mask id="path-11-inside-9_116_16" fill="white">
-          <rect x="168" y="187" width="26" height="17" rx="3" />
+        <rect x="168" y="184" width="26" height="17" rx="3" fill="#EAEAEA" stroke="#0F0F0F" stroke-width="10" mask="url(#path-10-inside-8_129_3)" />
+        <mask id="path-11-inside-9_129_3" fill="white">
+          <rect x="10" y="184" width="26" height="17" rx="3" />
         </mask>
-        <rect x="168" y="187" width="26" height="17" rx="3" fill="#9F9696" stroke="#0F0F0F" strokeWidth="10" mask="url(#path-11-inside-9_116_16)" />
-        <mask id="path-12-inside-10_116_16" fill="white">
-          <rect x="10" y="187" width="26" height="17" rx="3" />
+        <rect x="10" y="184" width="26" height="17" rx="3" fill="#EAEAEA" stroke="#0F0F0F" stroke-width="10" mask="url(#path-11-inside-9_129_3)" />
+        <circle cx="167.5" cy="33.5" r="8" fill="#282828" stroke="black" stroke-width="5" />
+        <circle cx="125.5" cy="33.5" r="8" fill="#282828" stroke="black" stroke-width="5" />
+        <circle cx="77.5" cy="33.5" r="8" fill="#282828" stroke="black" stroke-width="5" />
+        <circle cx="35.5" cy="33.5" r="8" fill="#282828" stroke="black" stroke-width="5" />
+        <mask id="path-16-inside-10_129_3" fill="white">
+          <rect x="23" y="128" width="157" height="39" rx="3" />
         </mask>
-        <rect x="10" y="187" width="26" height="17" rx="3" fill="#9F9696" stroke="#0F0F0F" strokeWidth="10" mask="url(#path-12-inside-10_116_16)" />
-        <circle cx="167.5" cy="36.5" r="8" fill="#282828" stroke="black" strokeWidth="5" />
-        <circle cx="125.5" cy="36.5" r="8" fill="#282828" stroke="black" strokeWidth="5" />
-        <circle cx="77.5" cy="36.5" r="8" fill="#282828" stroke="black" strokeWidth="5" />
-        <circle cx="35.5" cy="36.5" r="8" fill="#282828" stroke="black" strokeWidth="5" />
+        <rect x="23" y="128" width="157" height="39" rx="3" stroke="#0F0F0F" stroke-width="10" mask="url(#path-16-inside-10_129_3)" />
         <defs>
-          <linearGradient id="paint0_linear_116_16" x1="102" y1="13" x2="102" y2="259.5" gradientUnits="userSpaceOnUse">
-            <stop stopColor="#797979" />
-            <stop offset="1" stopColor="white" stopOpacity="0" />
+          <linearGradient id="paint0_linear_129_3" x1="102" y1="10" x2="102" y2="256.5" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#ABABAB" />
+            <stop offset="1" stop-color="white" stop-opacity="0" />
           </linearGradient>
-          <linearGradient className="oven-gradient" id="paint1_linear_116_16" x1="102" y1="170" x2="102" y2="78" gradientUnits="userSpaceOnUse">
-            <stop stopColor="#FF0000" />
-            <stop offset="1" stopOpacity="0" />
+          <linearGradient className="oven-gradient" id="paint1_linear_129_3" x1="102" y1="167" x2="102" y2="75" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#FF0000" />
+            <stop offset="1" stop-opacity="0" />
           </linearGradient>
         </defs>
       </svg>
-    </>
+    </div>
   );
 }
 
